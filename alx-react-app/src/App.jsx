f19cb6a56d62040d8fdcd5942b77d290c3147d51
JOE <joelmaniriho4@gmail.com>
2024-11-10 20:37:53 +0200

    created dev

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
     return (
         <div className="App">
-            <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
