0797a0ce6dc3379d749589d608676bbf9a272713
JOE <joelmaniriho4@gmail.com>
2024-11-17 17:37:26 +0200

    some

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -21,4 +21,17 @@ function App() {
     );
 }
 
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+    return (
+        <UserContext.Provider value={userData}>
+            <ProfilePage />
+        </UserContext.Provider>
+    );
+}
+
 export default App;
