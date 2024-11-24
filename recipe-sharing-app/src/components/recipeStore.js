a787c05f7bc245d577ebb4f2001f8858aebd1c40
JOE <joelmaniriho4@gmail.com>
2024-11-24 16:22:27 +0200

    14

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,20 +1,21 @@
 import create from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  setSearchTerm: (term) =>
+    set((state) => {
+      // Update search term and trigger filtering
+      const lowerCaseTerm = term.toLowerCase();
+      const filtered = state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(lowerCaseTerm)
+      );
+      return { searchTerm: term, filteredRecipes: filtered };
+    }),
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.filteredRecipes, newRecipe],
     })),
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-  setRecipes: (recipes) => set({ recipes }),
 }));
