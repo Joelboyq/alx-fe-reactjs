2ee2e3d0c9fa0ec0a97b896ac25fc825dd2e0bcc
JOE <joelmaniriho4@gmail.com>
2024-11-30 22:29:09 +0200

    2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,8 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile/*" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+};
+
+
   const [count, setCount] = useState(0)
 
   return (
