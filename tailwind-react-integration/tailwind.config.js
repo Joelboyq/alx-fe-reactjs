1fd1cb9e068c42d834b5eddf81758e8cba10c5c3
JOE <joelmaniriho4@gmail.com>
2024-12-01 21:28:12 +0200

    2

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Specify paths to all files using Tailwind classes
+  ],
+  theme: {
+    extend: {}, // Customize the default theme if needed
+  },
+  plugins: [], // Add any required plugins
+
+  darkMode: 'class', // Use 'class' to toggle dark mode with a class
+   
+  
+};
