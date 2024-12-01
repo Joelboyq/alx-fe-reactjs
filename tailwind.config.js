2d370748160a40af8d3cbb571692a940c27daf0b
JOE <joelmaniriho4@gmail.com>
2024-12-01 20:16:48 +0200

    7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans these files for class usage
+  ],
+  theme: {
+    extend: {}, // Use this to customize the default Tailwind theme
+  },
+  plugins: [], // Add custom plugins here
 };
-
